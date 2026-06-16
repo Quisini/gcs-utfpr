90a32c59dfcc08e420660368f7c20e0b8e3b19f4
Bernardo Cecconi Pereira <bernardoplaygm@gmail.com>
2026-06-15 22:18:24 -0300

    feat: adicionar classe Departamento

---
+++ Departamento.java
@@ -0,0 +1,9 @@
+public class Departamento {
+
+    private String nome;
+    private int codigo;
+
+    public void exibirDepartamento() {
+        System.out.println("Departamento: " + nome);
+    }
+}
