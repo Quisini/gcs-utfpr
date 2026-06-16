7658b3edf285f1d76dff3572809f691c496ee404
Bernardo Cecconi Pereira <bernardoplaygm@gmail.com>
2026-06-15 22:18:01 -0300

    feat: adicionar classe Pagamento

---
+++ Pagamento.java
@@ -0,0 +1,10 @@
+public class Pagamento {
+
+    private int idPagamento;
+    private double valor;
+    private String formaPagamento;
+
+    public void processarPagamento() {
+        System.out.println("Pagamento processado!");
+    }
+}
